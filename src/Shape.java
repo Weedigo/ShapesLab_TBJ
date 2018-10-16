ee12e193020376e531dc449405f5f47d58028da0
lostkid456 <jzhao7688@bths.edu>
2018-10-16 14:26:03 -0400

    Cool

--- src/Shape.java
+++ src/Shape.java
@@ -1,3 +1,18 @@
-public class Shape {
+public abstract class Shape
+{
+    private String type;
+
+    public Shape(String type)
+    {
+        this.type=type;
+    }
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+
+    public String toString()
+    {
+        return"I am a "+type+".";
+    }
 
 }
